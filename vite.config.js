b96548f947d18e2be23baed9d2cac99835d4b5ff
Azzam Khalaf <azzam.khalaf.swd23@gmail.com>
2026-04-18 10:35:37 +0200

    Fix: Configure Vite for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        demo: resolve(__dirname, 'view-demo.html')
+      }
+    }
+  }
+})
